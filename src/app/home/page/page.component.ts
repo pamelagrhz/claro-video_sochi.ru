e943a2b4bb9e48c6de353b6f7c5ee0d2b2fd3a2d
Pamela Ruíz <pamelaruiz@MacBook-Air-de-Pamela.local>
2020-07-14 20:42:25 -0500

    countdown logic

--- src/app/home/page/page.component.ts
+++ src/app/home/page/page.component.ts
@@ -1,21 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { HomeService } from './../home.service';
 import * as countdown from 'countdown';
-
+interface Time{
+  days: number,
+  hours:number,
+  minutes: number,
+  seconds: number
+}
 @Component({
   selector: 'app-page',
   templateUrl: './page.component.html',
   styleUrls: ['./page.component.css']
 })
 export class PageComponent implements OnInit {
+  time: Time =null;
 
   constructor(private homeService: HomeService) { }
 
   ngOnInit(): void {
-    var fechahora =new Date();
     const date = new Date('2020-07-25');
     date.setHours(0);
-    console.log(countdown(fechahora, date));
+    countdown(date,(ts)=>{
+      this.time =ts;
+    },countdown.DAYS|countdown.HOURS|countdown.MINUTES|countdown.SECONDS)
+    
   }
 
 }
