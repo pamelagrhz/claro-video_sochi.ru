eca2db50b2659551eb3e6b36e0a20bab503d6d4d
Pamela Ruíz <pamelaruiz@MacBook-Air-de-Pamela.local>
2020-07-14 21:49:22 -0500

    hover logic

--- src/app/home/page/page.component.ts
+++ src/app/home/page/page.component.ts
@@ -1,29 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { HomeService } from './../home.service';
 import * as countdown from 'countdown';
+import { ArrayType } from '@angular/compiler';
 interface Time{
   days: number,
   hours:number,
   minutes: number,
   seconds: number
 }
+
 @Component({
   selector: 'app-page',
   templateUrl: './page.component.html',
   styleUrls: ['./page.component.css']
+  
 })
 export class PageComponent implements OnInit {
   time: Time =null;
-
+  
+  
   constructor(private homeService: HomeService) { }
-
-  ngOnInit(): void {
+ 
+  ngOnInit(): void {    
     const date = new Date('2020-07-25');
     date.setHours(0);
     countdown(date,(ts)=>{
       this.time =ts;
     },countdown.DAYS|countdown.HOURS|countdown.MINUTES|countdown.SECONDS)
-    
+ 
   }
 
 }
